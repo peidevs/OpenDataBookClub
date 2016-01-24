75cedf5d86be6da5d55418bfa4e63a2b243fae82
Michael Easter <codetojoy@gmail.com>
2016-01-24 11:58:06 -0400

    incremental change as part of a frenzy of activity

---
+++ tools/PDF_Thresher/setvars.bat
@@ -0,0 +1,7 @@
+
+set CLASSPATH=%CLASSPATH%:bin
+set CLASSPATH=%CLASSPATH%:lib/groovy-all-2.4.5.jar
+set CLASSPATH=%CLASSPATH%:lib/pdfbox-2.0.0-RC2.jar
+set CLASSPATH=%CLASSPATH%:lib/commons-logging-1.2.jar
+set CLASSPATH=%CLASSPATH%:lib/fontbox-2.0.0-RC2.jar
+
